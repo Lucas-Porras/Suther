8429c77014346f141751387873fe59ec3a7f8eaa
unknown <abdhalalucas@gmail.com>
2022-06-10 11:41:37 -0300

    Hasta aca mi parte asdjkasd

--- database/database.php
+++ database/database.php
@@ -1,9 +1,14 @@
 <?php
-    define("", DB_SERVER);
-    define("", DB_USER);
+    define("localhost", DB_SERVER);
+    define("root", DB_USER);
     define("", DB_PASS);
-    define("", DB_NAME);
+    define("suther", DB_NAME);
 
-    $conexion = mysqli_connect();
-    mysqli_set_charset($)
+    $conexion = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    
+    mysqli_set_charset($conexion, "utf-8");
+
+    if(!$conexion){
+    	exit("Error en la conexion: " . mysqli_connect_error());
+    }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  require_once "database/database.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +15,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
-</hfead>
+</head>
 <body>
     <div>
         <nav class="navbar navbar-expand-lg shadow-sm">
