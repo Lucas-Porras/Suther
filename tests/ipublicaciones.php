3b1402b0bbc1778b430f60d30e354bd229e28434
CondoriAgustin <leonelagustin205@gmail.com>
2022-06-30 12:25:51 -0300

    Despues arreglo bien lo de imagenes publicaciones

--- tests/ipublicaciones.php
+++ tests/ipublicaciones.php
@@ -6,19 +6,20 @@
 <?php include_once "../partials/header.php" ?> 
 <body>
 	<div class="container mt-5">
-		<div class="principal p-3 shadow-lg rounded-2" id="iPublicaciones" style="background-color: red;">
-			<div class="d-flex">
-				<div class="img-pub">
-					<img src="https://www.lavanguardia.com/files/article_main_microformat/uploads/2019/10/04/5fa533342d51e.jpeg" style="min-width: 10%; max-width: 50%;">
-				</div>
-				<div class="text-img-pub justify-content-start">
+		<div class="principal p-2 shadow-lg rounded-2" id="iPublicaciones" style="background-color: red;">
+			<div class="d-flex p-2 shadow-lg rounded-2">
+				<img src="https://dam.ngenespanol.com/wp-content/uploads/2019/04/cordillera-andes.png" class="rounded-2"style="min-width:15%; max-width: 50%;">
+				<div class="text-img-pub p-2" style="background-color: yellow;">
 					<div class="d-flex">
 						<div class="img-profile">
 							Foto_Usuario
 						</div>
-						<div class="name-profile" style="padding-left:	 10	px;">
+						<div class="name-profile" style="padding-left:10px;">
 							Nombre_Usuario
 						</div>
+					</div>
+					<div class="p-2" style="background-color: white;">
+						xdx
 					</div>
 				</div>
 			</div>
